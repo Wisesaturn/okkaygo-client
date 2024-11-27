65474c472256f1725abca6eb2c369854d0d8ec4e
Jae Han Song <rfv1479@gmail.com>
2024-11-27 23:01:20 +0900

    chore : adjust local font (pretendard)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)'],
+      },
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
